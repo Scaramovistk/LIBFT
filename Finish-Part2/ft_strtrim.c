95cfb58850ea796921b991561afb75ae13338023
Scaramovistk <85752923+Scaramovistk@users.noreply.github.com>
2022-01-07 10:40:07 +0100

    Update ft_strtrim.c

--- Finish-Part2/ft_strtrim.c
+++ Finish-Part2/ft_strtrim.c
@@ -15,7 +15,7 @@
 
 size_t	ft_strlen(const char *str)
 {
-	size_t		size;
+	size_t	size;
 
 	size = 0;
 	while (str[size] != '\0')
@@ -23,65 +23,75 @@ size_t	ft_strlen(const char *str)
 	return (size);
 }
 
-void    *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t      i;
-    char        *dst;
-    char        *origin;
+	size_t	i;
+	char	*dst;
+	char	*origin;
 
-    if (dest == NULL && src == NULL)
-        return (NULL);
-    dst = (char *)dest;
-    origin = (char *)src;
-    i = 0;
-    while (n > i)
-    {
-        dst[i] = origin[i];
-        i++;
-    }
-    return (&dest[0]);
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	dst = (char *)dest;
+	origin = (char *)src;
+	i = 0;
+	while (n > i)
+	{
+		dst[i] = origin[i];
+		i++;
+	}
+	return (&dest[0]);
 }
 
-char    *ft_strtrim(char const *s1, char const *set)
+static char	*ft_trimmed(char const *set, char const *s1, char *dest)
 {
-    char *dest;
-    int row;
-    int a;
-    int find;
-    int lens1;
+	int	lens1;
+	int	row;
+	int	a;
+	int	find;
+	int	size;
 
-    row = 0;
-    lens1 = ft_strlen(s1);
-    dest = malloc(sizeof(char) * lens1 + 1);
-    if (dest == NULL)
-        return (NULL);
-    ft_memcpy(dest, s1, lens1);
-    while (dest[row] != '\0')
-    {
-        find = 0;
-        a = row;
-        while (dest[a] == set[find])
-        {   
-            if (set[find + 1] == '\0')
-                ft_memcpy(dest + row, dest + (row + ft_strlen(set)), lens1 - row);
-            find++;
-            a++;
-        }
-        row++;
-    }
-    return(dest);
+	row = 0;
+	lens1 = ft_strlen(s1);
+	while (dest[row] != '\0')
+	{
+		find = 0;
+		a = row;
+		size = lens1 - row;
+		while (dest[a] == set[find])
+		{
+			if (set[find + 1] == '\0')
+				ft_memcpy(dest + row, dest + (row + ft_strlen(set)), size);
+			find++;
+			a++;
+		}
+		row++;
+	}
+	return (dest);
 }
 
-
-#include <stdio.h>
-int     main (void)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    char s1[] = "Hello World";
-    char *dest;
-    int row = 7;
+	char	*dest;
+	int		lens1;
 
-    printf("\nBefore:%s\n", s1);
-    dest = ft_strtrim(s1, "o");
-    printf("\nAfter:%s\n", dest);
-    return (0);
+	lens1 = ft_strlen(s1);
+	dest = malloc(sizeof(char) * lens1 + 1);
+	if (dest == NULL)
+		return (NULL);
+	ft_memcpy(dest, s1, lens1);
+	ft_trimmed(set, s1, dest);
+	return (dest);
 }
+
+	#include <stdio.h>
+	int		main (void)
+	{
+		char	s1[] = "Hello World";
+		char	*dest;
+
+		printf("\nBefore:%s\n", s1);
+		dest = ft_strtrim(s1, "o");
+		printf("\nAfter:%s\n", dest);
+
+		return (0);
+	}
